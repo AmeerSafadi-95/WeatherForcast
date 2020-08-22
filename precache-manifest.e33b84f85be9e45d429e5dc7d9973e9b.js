5349a4379ef1c487b48df01619673ea4d51e64f3
AmeerSafadi-95 <ameersafadi95@gmail.com>
2020-08-22 19:47:33 +0300

    Updates

---
+++ precache-manifest.e33b84f85be9e45d429e5dc7d9973e9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15e08b6577f9455a8b0bfd79fe3dc5e2",
+    "url": "/WeatherForcast/index.html"
+  },
+  {
+    "revision": "ee9f098486ce913b03df",
+    "url": "/WeatherForcast/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "41c3ec706b0f77ecdee8",
+    "url": "/WeatherForcast/static/css/main.e7362764.chunk.css"
+  },
+  {
+    "revision": "ee9f098486ce913b03df",
+    "url": "/WeatherForcast/static/js/2.fe4d92a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WeatherForcast/static/js/2.fe4d92a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41c3ec706b0f77ecdee8",
+    "url": "/WeatherForcast/static/js/main.2b5100f2.chunk.js"
+  },
+  {
+    "revision": "1eb78ab3ad5c5ac8b5e0",
+    "url": "/WeatherForcast/static/js/runtime-main.bbfe4ede.js"
+  }
+]);
